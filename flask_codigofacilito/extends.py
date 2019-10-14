1c032aefe656fa8e72a57bea7a42697933764b1d
Cristian Vidal <cris.vidal04@gmail.com>
2019-10-13 23:30:55 -0300

    orden con los templates, pendiente formulario de contacto

--- flask_codigofacilito/extends.py
+++ flask_codigofacilito/extends.py
@@ -2,19 +2,31 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+# SERVICIO WEB
+# RUTAS Y PARAMATROS
+# VARIABLES, CICLOS Y CONDICIONES
+# TEMPLATE
 # HERENCIAS Y PLANTILLAS
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+ 
+@app.route('/usuario')
+def usuario():
     nombre_usuario = 'Cristian'
-    return render_template('index.html', nombre = nombre_usuario)
+    return render_template('user.html', nombre = nombre_usuario)
 
-@app.route('/client')
-def client():
+@app.route('/clientes')
+def clientes():
     nombres = ['cliente 1', 'cliete 2', 'cliente 3', 'cliente 4']
     return render_template('client.html', nombres_clientes = nombres)
+
+@app.route('/contacto')
+def contacto():
+    return 'pendiente pagina de contacto'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- flask_codigofacilito/run.py
+++ flask_codigofacilito/run.py
@@ -1,12 +1,32 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
-# INICIAR SERVIDOR
+# SERVICIO WEB
+# RUTAS Y PARAMATROS
+# VARIABLES, CICLOS Y CONDICIONES
+# TEMPLATE
+# HERENCIAS Y PLANTILLAS
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hola mundo'
+    return render_template('index.html')
+ 
+@app.route('/usuario')
+def usuario():
+    nombre_usuario = 'Cristian'
+    return render_template('user.html', nombre = nombre_usuario)
+
+@app.route('/clientes')
+def clientes():
+    nombres = ['cliente 1', 'cliete 2', 'cliente 3', 'cliente 4']
+    return render_template('client.html', nombres_clientes = nombres)
+
+@app.route('/contacto')
+def contacto():
+    return 'Pagina de contacto pendiente'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
