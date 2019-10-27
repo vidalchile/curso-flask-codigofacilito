10864599f197b487cf93deb9ba290cb529a607b4
Cristian Vidal <cris.vidal04@gmail.com>
2019-10-27 19:34:09 -0300

    formularios y validaciones

--- flask_codigofacilito/forms.py
+++ flask_codigofacilito/forms.py
@@ -1,9 +1,41 @@
 from wtforms import Form, BooleanField, StringField, TextField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
+from wtforms import HiddenField
 
 # Este archivo prodría tener todos los formularios
 
+# Validaciones propias
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('Error campo debe estar vacío.')
+
+class LoginForm(Form):
+    username = StringField('username',
+    [
+        validators.Required(),
+        validators.Length(min=4, max=25, message='Ingrese un username valido!')
+    ])
+
+    password = PasswordField('Contraseña', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords no coincide')
+    ])
+
+    confirm = PasswordField('Repetir contraseña')
+
 class CommentForm(Form):
-    username = StringField('username')
-    email = EmailField('Correo electronico')
+    
+    username = StringField('username', 
+    [
+        validators.Required(),
+        validators.Length(min=4, max=25, message='Ingrese un username valido!')
+    ])
+
+    email = EmailField('Correo electronico',
+    [
+        validators.Required(),
+        validators.Email(message='Ingrese un email valido!')
+    ])
     comment = TextField('Comentario')
+
+    honeypot = HiddenField('', [length_honeypot])
--- flask_codigofacilito/run.py
+++ flask_codigofacilito/run.py
@@ -1,14 +1,24 @@
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_wtf.csrf import CSRFProtect
+import forms
 
 # SERVICIO WEB
 # RUTAS Y PARAMATROS
 # VARIABLES, CICLOS Y CONDICIONES
 # TEMPLATE
 # HERENCIAS Y PLANTILLAS
+# FORMULARIOS
+# VALIDACIONES FORMULARIOS
+# VALIDACIONES PROPIAS, CAMPOS HIDDEN
+# CSRF Protection
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key' # Identificador unico
+csrf = CSRFProtect(app)
 
 @app.route('/')
 def index():
@@ -24,9 +34,28 @@ def clientes():
     nombres = ['cliente 1', 'cliete 2', 'cliente 3', 'cliente 4']
     return render_template('client.html', nombres_clientes = nombres)
 
-@app.route('/contacto')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    formulario_login = forms.LoginForm(request.form)
+    
+    if request.method == 'POST' and formulario_login.validate():
+        print(formulario_login.username.data)
+        print(formulario_login.password.data)
+
+    titulo = 'Login'
+    return render_template('login.html', title=titulo, form = formulario_login)
+
+@app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
-    return 'Pagina de contacto pendiente'
+    formulario = forms.CommentForm(request.form)
+    
+    if request.method == 'POST' and formulario.validate():
+        print(formulario.username.data)
+        print(formulario.email.data)
+        print(formulario.comment.data)
+
+    titulo = 'Formulario'
+    return render_template('formulario.html', title=titulo, form = formulario)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
